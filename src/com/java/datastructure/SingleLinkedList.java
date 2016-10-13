ef7b94553a7f10e1c193e5f162bbe25fd68e1938
Tamilselvan Radhakrishnan <rkag1944@gmail.com>
2016-10-12 23:47:52 -0400

    Initial push

---
+++ src/com/java/datastructure/SingleLinkedList.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java.datastructure;
+
+/**
+ * @author tamil
+ *
+ */
+public class SingleLinkedList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
